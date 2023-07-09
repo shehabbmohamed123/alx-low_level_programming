21612500d36a585356f4566f05171f495660d270
shehabbmohamed123 <shehab12p@gmail.com>
2023-07-09 09:54:13 -0700

    a

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <ctype.h>
+#include <stdbool.h>
 /**
  * coinConverter - Helper function that does all the mathematics
  * @i: Passed in variable from main for calculations
