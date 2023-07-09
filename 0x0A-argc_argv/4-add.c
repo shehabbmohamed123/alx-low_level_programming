bcad04deb87f29c6301f59b42b257af5015f3401
shehabbmohamed123 <shehab12p@gmail.com>
2023-07-09 09:53:22 -0700

    a

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
+#include <ctype.h>
 /**
  * main - Program that takes in all integer arguments and returns the sum
  * @argc: Number of command line arguments
