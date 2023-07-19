091d40d00b0649602ad7c87a54b3ab247223df2f
shehabbmohamed123 <shehab12p@gmail.com>
2023-07-19 13:09:26 -0700

    a

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+#ifndef FUNCION_POINTERS_H
+#define FUNCION_POINTERS_H
+
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
