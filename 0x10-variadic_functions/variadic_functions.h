92152774c2173f2d5eedd4d690a5cb2fb03b2c08
shehabbmohamed123 <shehab12p@gmail.com>
2023-07-20 16:50:41 -0700

    a

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -10,6 +10,17 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
 
+/**
+ * struct format - Data type of a format.
+ * @op: Format.
+ * @f: Function.
+ *
+ */
 
+typedef struct format
+{
+	char *op;
+	void (*f)(va_list all);
+} f;
 
 #endif
