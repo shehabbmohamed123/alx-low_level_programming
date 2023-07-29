cf8c1aa5422db78cf68ede35bc681f7f4e7b5862
shehabbmohamed123 <shehab12p@gmail.com>
2023-07-28 17:26:59 -0700

    a

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 #include "lists.h"
+void _constructor(void) __attribute__((constructor));
 
 /**
- * before_main - funcion that prints a specific string before the main,
- * function in executed.
+ * _constructor - executes defore main()
  *
- * Return: no return.
+ * Return: void
  */
 
 
-void before_main(void)
+void _constructor(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+	I bore my house upon my back!\n");
 }
